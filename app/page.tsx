d9c67aa6eea5012c4477ca6ba38cef716dc4d79d
Chris YESSO <yessochrisa@gmail.com>
2024-04-19 15:55:24 +0000

    Loading state for logins

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Icon } from "@iconify/react/dist/iconify.js"
+import { Loader2 } from "lucide-react"
 import { signIn, signOut, useSession } from "next-auth/react"
 
 
@@ -9,15 +11,18 @@ export default function Home() {
     const { data: session, status } = useSession()
     return (
         <main className="p-4 flex flex-1 flex-col items-center text-center justify-center h-full gap-4 w-full">
+            <Badge variant="secondary">A goofy fantasy economy game</Badge>
             <h1 className="text-4xl font-bold">Trade the worst possible stonks.</h1>
             {
-                session ? (
-                    <Button variant="default" onClick={() => signOut()}>
-                        Log out <Icon icon="akar-icons:arrow-right" className="ml-2" /></Button>
-                ) : (
-                    <Button variant="default" onClick={() => signIn('reddit')}>
-                        Login with Reddit <Icon icon="basil:reddit-solid" className="ml-2" /></Button>
-                )
+                status == "loading" ? <Button disabled variant="ghost">
+                    Please wait <Loader2 size={12} className="animate-spin ml-2" /></Button>
+                    : session ? (
+                        <Button onClick={() => signOut()}>
+                            Log out <Icon fontSize={12} icon="akar-icons:arrow-right" className="ml-2" /></Button>
+                    ) : (
+                        <Button variant="default" onClick={() => signIn('reddit')}>
+                            Login with Reddit <Icon fontSize={12} icon="basil:reddit-solid" className="ml-2" /></Button>
+                    )
             }
 
         </main>

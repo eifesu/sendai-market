feda44d358eeba18cfabecd0b7a6ae6131313131
Chris YESSO <yessochrisa@gmail.com>
2024-04-19 15:26:57 +0000

    Added Reddit login

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 
 import { Button } from "@/components/ui/button"
 import { Icon } from "@iconify/react/dist/iconify.js"
+import { signIn } from "next-auth/react"
 
 
 export default function Home() {
     return (
         <main className="p-4 flex flex-1 flex-col items-center text-center justify-center h-full gap-4 w-full">
             <h1 className="text-4xl font-bold">Trade the worst possible stonks.</h1>
-            <Button variant="default">
+            <Button variant="default" onClick={() => signIn('reddit')}>
                 Login with Reddit <Icon icon="basil:reddit-solid" className="ml-2" /></Button>
         </main>
     )
